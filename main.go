19d7b700e22159e3c7eb3e03d2b2039758c7d780
Robbie08 <rortiz8838@sdsu.edu>
2021-08-20 21:24:11 -0700

    Update: Created new button to gracefully shutdown server and updated some logs

--- main.go
+++ main.go
@@ -1,32 +1,44 @@
 package main
 
 import (
-	"github.com/Robbie08/GoPiTest/piUtils"
-	"log"
+	"fmt"
+	"github.com/Robbie08/GoPiTest/pkg/piUtils"
+	log "github.com/sirupsen/logrus"
 	"net/http"
-	"os/exec"
+	"os"
 )
 
 func main() {
-	http.HandleFunc("/", defaultPage)
+	log.SetFormatter(&log.JSONFormatter{})
+	log.Info("Starting server...")
+    http.HandleFunc("/", defaultPage)
 	http.HandleFunc("/on", onState)
 	http.HandleFunc("/off", offState)
+	http.HandleFunc("/shutdown", shutdown)
 	http.ListenAndServe(":8080", nil)
 }
 
-func defaultPage(w http.ResponseWritter, r *http.Request) {
-	http.ServerFile(w, r, "homePage.html")
-	fmt.Println("Default Page")
+func defaultPage(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		log.Info("Page not found")
+		return
+	}
+	log.Info("Someone hit the Default Page...")
+	http.ServeFile(w, r, "piInterface.html")
 }
-
-func onState(w http.ResponseWritter, r *http.Request) {
-	http.ServerFile(w, r, "goButtons.html")
+func shutdown(w http.ResponseWriter, r *http.Request) {
+	log.Info("Shutting server down...")
+	os.Exit(0)
+}
+func onState(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "piInterface.html")
 	fmt.Println("State: ON")
-	piUtils.SendSignalToGPIO(1)
+	piUtils.SendSignalToGPIO("1")
 }
 
-func offState(w http.ResponseWritter, r *http.Request) {
-	http.ServerFile(w, r, "goButtons.html")
+func offState(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "piInterface.html")
 	fmt.Println("State: OFF")
-	piUtils.SendSignalToGPIO(0)
+	piUtils.SendSignalToGPIO("0")
 }
